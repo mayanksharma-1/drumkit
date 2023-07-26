5da415c0bac85234f6013065cf4788c016184183
Mayank Sharma <as.mayanksharma@gmail.com>
2023-07-26 23:59:10 +0530

    fix audio reset bug

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 window.addEventListener('keydown',function(e){
     const audio = document.querySelector(`audio[data-key=${e.code}]`);
-    if (audio){ audio.play();}
+    if (!audio){ return;} // to prevent error if a key that doesn't have a data-key assigned to it is pressed 
+    else{
+        audio.currentTime = 0; // reset the sound currently playing when another is clicked
+        audio.play();
+        }
 });
